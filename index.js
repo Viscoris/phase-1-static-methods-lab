5a8818a2caf559cbaec18d8ff516a9b41fe6aff4
Daniel Waghorne <dwaghorne@hotmail.com>
2024-02-11 16:02:28 +1000

    lab

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\-\'\s]/g, '');
+  }
+
+  static titleize(string) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    
+    const words = string.split(' ');
+
+ 
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !smallWords.includes(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word.toLowerCase();
+      }
+    });
+
+    
+    const titleizedString = titleizedWords.join(' ');
+
+    return titleizedString;
+  }
 }
+
+
+
